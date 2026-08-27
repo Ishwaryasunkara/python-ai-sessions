bb270ea2ec767fc50f898ec0b6ec4ca35987fc24
AakashRathor <aakashrathor2019@gmail.com>
2026-08-27 07:13:34 +0530

    test: implement test cases for users and policies suites

---
+++ user_policy_management_system/tests/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+
+from app.database.database import Base, get_db
+from app.main import app
+
+
+test_engine = create_engine(
+    "sqlite://",
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=test_engine,
+)
+
+
+@pytest.fixture
+def client():
+    Base.metadata.create_all(bind=test_engine)
+    db = TestingSessionLocal()
+
+    def override_get_db():
+        yield db
+
+    app.dependency_overrides[get_db] = override_get_db
+
+    with TestClient(app) as test_client:
+        yield test_client
+
+    app.dependency_overrides.clear()
+    db.close()
+    Base.metadata.drop_all(bind=test_engine)
